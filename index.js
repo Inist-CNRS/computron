145ebddd576fc13fea34116c90b91cafb9fd1bfc
Rémy MEJA <remymeja@free.fr>
2018-09-27 15:34:59 +0200

    delete test code and fix index.js for better exports

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
 const addon = require('bindings')('addon');
-const transformer = new addon.Transformer();
-console.log(transformer);
-transformer.loadStylesheet('dataset/otherxslt.xsl');
-transformer.apply('dataset/draft.xml', (error, result) => {
-  if (error) return console.error('error', error);
-  console.log('result : ', result);
-});
-
-console.log('dude ?');
+module.exports = addon.Transformer;
